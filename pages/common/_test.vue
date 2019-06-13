af559e84116e44dceb0d047b799868ce28e9278c
huwei <huw@citydo.com.cn>
2019-06-13 14:48:32 +0800

    change:init

---
+++ pages/common/_test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{xx}}</div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class TestComponent extends Vue {
+  @Prop({ type: String }) post!: "{ a: 1 }";
+  private xx = 1;
+  constructor() {
+    super();
+  }
+  //   this.
+}
+</script>
+
+
